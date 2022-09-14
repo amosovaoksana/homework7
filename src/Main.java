36e8ab6ae27e09b8493940850439077f69173f3d
homework2 <amosovaoksana16@gmail.com>
2022-09-14 22:53:45 +0500

    one commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,44 @@ public class Main {
     public static void main(String[] args) {
      //Задание 1
         System.out.println("Задача 1");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 1; total < 2459000; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц" +i + "Сумма накоплений равна " + total + "рублей");
+        }
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
 }
